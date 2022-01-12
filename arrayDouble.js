19cc417f754738d2b67a655043b5af937633832a
YogendraRajpoot <yogendrarajpooten1030@gmail.com>
2022-01-13 00:36:51 +0530

    first commit

---
+++ arrayDouble.js
@@ -0,0 +1,8 @@
+// Given an array of numbers generate an 
+// array containing the double value of the numbers
+
+var arr = [2,3,5,7,9]
+const arrayDouble = x => x=x*2 
+
+console.log(arr.map(arrayDouble))
+
